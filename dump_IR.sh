f50f92812abe0fac58901c0ad7911ab2104db9c6
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2020-03-22 13:34:51 +0100

    Renamed basic pass, that just dumps IR

--- dump_IR.sh
+++ dump_IR.sh
@@ -4,4 +4,4 @@
 #OMPI_CXX=clang++ mpicxx -fopenmp -Xclang -load -Xclang build/experimentpass/libexperimentpass.so  $1
 
 # using mpich:
-$MPICXX -cxx=clang++ -fopenmp -Xclang -load -Xclang build/experimentpass/libexperimentpass.so  $1
+$MPICXX -cxx=clang++ -fopenmp -Xclang -load -Xclang build/dump_ir_pass/libdump_ir_pass.so  $1
