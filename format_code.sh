aff77963f76188ac96ff1f68620b92f29fa4a663
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2020-03-22 20:49:48 +0100

    included small utility script to format code

---
+++ format_code.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#clang-format --style=llvm -i ./tests/*.cpp ./tests/*.h
+#clang-format --style=llvm -i ./msg_order_relax_checker/*.cpp ./msg_order_relax_checker/*.h
+#clang-format --style=llvm -i ./dump_ir_pass/*.cpp ./dump_ir_pass/*.h
+
+clang-format --style=llvm -i ./tests/*.c
+clang-format --style=llvm -i ./msg_order_relax_checker/*.cpp ./msg_order_relax_checker/*.h
+clang-format --style=llvm -i ./dump_ir_pass/*.cpp
