61f756b4d71a38e0da11fdae0b438331ba0ae9f5
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2020-03-25 15:38:12 +0100

    adopted script to run pass to either invoce clang or clang++ depending on file suffix

--- run.sh
+++ run.sh
@@ -4,4 +4,10 @@
 #OMPI_CXX=clang++ mpicxx -fopenmp -Xclang -load -Xclang build/experimentpass/libexperimentpass.so  $1
 
 # using mpich:
+if [ ${1: -2} == ".c" ]; then
+$MPICC -cc=clang -fopenmp -Xclang -load -Xclang build/msg_order_relax_checker/libmsg_order_relax_checker.so  $1
+elif [ ${1: -4} == ".cpp" ]; then
 $MPICXX -cxx=clang++ -fopenmp -Xclang -load -Xclang build/msg_order_relax_checker/libmsg_order_relax_checker.so  $1
+else
+echo "Unknown file suffix, use this script with .c or .cpp files"
+fi
