2ca9e5ee11f998239436bbd06b0764a81c02bd1a
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2020-03-27 13:37:57 +0100

    also formating the new testcases

--- format_code.sh
+++ format_code.sh
@@ -5,5 +5,6 @@
 #clang-format --style=llvm -i ./dump_ir_pass/*.cpp ./dump_ir_pass/*.h
 
 clang-format --style=llvm -i ./tests/*.c
+clang-format --style=llvm -i ./tests/two_messages/*.c
 clang-format --style=llvm -i ./msg_order_relax_checker/*.cpp ./msg_order_relax_checker/*.h
 clang-format --style=llvm -i ./dump_ir_pass/*.cpp
