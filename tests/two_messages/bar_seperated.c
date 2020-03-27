6d186a5a3b0abdb567fd6b9e24bb66d99210a126
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2020-03-27 13:20:57 +0100

    fixed barrier testcases

--- tests/two_messages/bar_seperated.c
+++ tests/two_messages/bar_seperated.c
@@ -1,8 +1,7 @@
 #include <mpi.h>
 #include <stdio.h>
 
-#define MSG_TAG_A 123
-#define MSG_TAG_B 1234
+#define MSG_TAG 123
 #define N 1000
 
 int main() {
@@ -13,13 +12,13 @@ int main() {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   if (rank == 0) {
-    MPI_Recv(&a, 1, MPI_INT, 1, MSG_TAG_A, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Recv(&a, 1, MPI_INT, 1, MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Recv(&b, 1, MPI_INT, 1, MSG_TAG_B, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Recv(&b, 1, MPI_INT, 1, MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   } else if (rank == 1) {
-    MPI_Send(&a, 1, MPI_INT, 0, MSG_TAG_A, MPI_COMM_WORLD);
+    MPI_Send(&a, 1, MPI_INT, 0, MSG_TAG, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Send(&b, 1, MPI_INT, 0, MSG_TAG_B, MPI_COMM_WORLD);
+    MPI_Send(&b, 1, MPI_INT, 0, MSG_TAG, MPI_COMM_WORLD);
   }
   MPI_Finalize();
 }
