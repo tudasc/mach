23eb2903110c0711e06e68623005fcac84995332
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2020-03-30 16:54:33 +0200

    another testcase

---
+++ tests/hidden_in_functions/send_2.c
@@ -0,0 +1,30 @@
+#include <mpi.h>
+#include <stdio.h>
+
+#define MSG_TAG 123
+#define N 1000
+
+__attribute__((noinline)) void send() {
+  int b = 2;
+  MPI_Send(&b, 1, MPI_INT, 0, MSG_TAG, MPI_COMM_WORLD);
+}
+
+int main() {
+  int a = 1;
+  int b = 2;
+
+  MPI_Init(NULL, NULL);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  switch (rank) {
+  case 0:
+    MPI_Recv(&a, 1, MPI_INT, 1, MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Recv(&b, 1, MPI_INT, 1, MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    break;
+  case 1:
+    send();
+    MPI_Send(&a, 1, MPI_INT, 0, MSG_TAG, MPI_COMM_WORLD);
+    break;
+  }
+  MPI_Finalize();
+}
