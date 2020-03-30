5f175b0dd962d034918b163b0ed87a41dec17ebc
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2020-03-30 15:37:09 +0200

    testcases for function analysis

--- format_code.sh
+++ format_code.sh
@@ -6,5 +6,6 @@
 
 clang-format --style=llvm -i ./tests/*.c
 clang-format --style=llvm -i ./tests/two_messages/*.c
+clang-format --style=llvm -i ./tests/hidden_in_functions/*.c
 clang-format --style=llvm -i ./msg_order_relax_checker/*.cpp ./msg_order_relax_checker/*.h
 clang-format --style=llvm -i ./dump_ir_pass/*.cpp
---
+++ tests/hidden_in_functions/bar.c
@@ -0,0 +1,31 @@
+#include <mpi.h>
+#include <stdio.h>
+
+#define MSG_TAG 123
+#define N 1000
+
+__attribute__((noinline)) void call_barrier() { MPI_Barrier(MPI_COMM_WORLD); }
+
+// barrier seperates the msg
+
+int main() {
+  int a = 1;
+  int b = 2;
+
+  MPI_Init(NULL, NULL);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  switch (rank) {
+  case 0:
+    MPI_Recv(&a, 1, MPI_INT, 1, MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Recv(&b, 1, MPI_INT, 1, MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    break;
+  case 1:
+    MPI_Send(&a, 1, MPI_INT, 0, MSG_TAG, MPI_COMM_WORLD);
+    call_barrier();
+    MPI_Send(&b, 1, MPI_INT, 0, MSG_TAG, MPI_COMM_WORLD);
+    break;
+  }
+  MPI_Finalize();
+}
