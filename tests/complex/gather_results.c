231e221466c19a6f21fc656b08defce87ca62c5f
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2020-04-02 17:30:34 +0200

    more tests

---
+++ tests/complex/gather_results.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MSG_TAG 123
+#define N 1000
+
+// no dependency
+
+// process 0 gatheres some data
+int main() {
+
+  int data;
+
+  MPI_Init(NULL, NULL);
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  data = rank;
+
+  if (rank == 0) {
+    printf("%d ", data);
+    for (int i = 1; i < size; ++i) {
+      MPI_Recv(data, 1, MPI_INT, i, MSG_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      printf("%d ", data);
+    }
+  } else {
+    MPI_Send(&data, 1, MPI_INT, 0, MSG_TAG, MPI_COMM_WORLD);
+  }
+
+  MPI_Finalize();
+}
---
+++ tests/complex/gather_results_nondeterminally.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MSG_TAG 123
+#define N 1000
+
+// no dependency, this in nondeterministic according to the current dtandard
+// anyway
+
+// process 0 gatheres some data
+int main() {
+
+  int data;
+
+  MPI_Init(NULL, NULL);
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  data = rank;
+
+  if (rank == 0) {
+    printf("%d ", data);
+    for (int i = 1; i < size; ++i) {
+      MPI_Recv(data, 1, MPI_INT, MPI_ANY_SOURCE, MSG_TAG, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      printf("%d ", data);
+    }
+  } else {
+    MPI_Send(&data, 1, MPI_INT, 0, MSG_TAG, MPI_COMM_WORLD);
+  }
+
+  MPI_Finalize();
+}
