a70a60335f5f1d807b8972e29e99d1eedf8d0b25
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2020-04-15 16:16:48 +0200

    renamed the pass

--- format_code.sh
+++ format_code.sh
@@ -9,5 +9,5 @@ clang-format --style=llvm -i ./tests/two_messages/*.c
 clang-format --style=llvm -i ./tests/more_msg/*.c
 clang-format --style=llvm -i ./tests/hidden_in_functions/*.c
 clang-format --style=llvm -i ./tests/complex/*.c
-clang-format --style=llvm -i ./msg_order_relax_checker/*.cpp ./msg_order_relax_checker/*.h
+clang-format --style=llvm -i ./mpi_assertion_checker/*.cpp ./msg_order_relax_checker/*.h
 clang-format --style=llvm -i ./dump_ir_pass/*.cpp
