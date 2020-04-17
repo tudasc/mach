2bb0b8a93d83c0d8fb1b9f5abb0f58f65882b9f3
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2020-04-17 10:41:44 +0200

    added debug Macro

---
+++ mpi_assertion_checker/debug.h
@@ -0,0 +1,28 @@
+/*
+  Copyright 2020 Tim Jammer
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+#ifndef MACH_DEBUG_H
+#define MACH_DEBUG_H
+
+#define DEBUG_MACH_PASS 1
+
+#if DEBUG_MACH_PASS == 1
+#define Debug(x) x
+#else
+#define Debug(x)
+#endif
+
+#endif
